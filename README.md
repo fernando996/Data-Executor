ead489b1b830886276ff546a0854accb40552f8c
Fernando Martins <fernando.martins@iem.madeira.gov.pt>
2023-03-20 14:22:23 +0000

    add config

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Process data on a folder using an python script with different configuration and
 
 To run script:
 python script.py -d C:\Users\Fernando\Videos\smartdest\480p\teste
+
+python script.py -d "C:\Users\fernandom\Videos\teste"
--- script.py
+++ script.py
@@ -1,16 +1,13 @@
 import os
 import argparse
 import ffmpeg
-
-CONFIGS = ["width","height", "display_aspect_ratio","avg_frame_rate", "duration"]
-# FILTERS = {"fps":[25,20,15,10,5,3,2,1], "width": [1920, 1280, 640, 480,240]}
-FILTERS = {"fps":[25,2,1], "scale": [ {"w":1920, "h": 1080}, {"w":1280, "h": 720} , {"w":640, "h": 480} ]}
-
+import config
+import subprocess
 
 ap = argparse.ArgumentParser()
 
 ap.add_argument("-d", "--directory", required=True,
-    help="path to directory with files")
+                help="path to directory with files")
 
 # ap.add_argument("-o", "--output", type=str,
 #     help="path to optional output video file")
@@ -23,12 +20,12 @@ args = vars(ap.parse_args())
 
 # Get the list of all files and directories
 dir_list = os.listdir(args["directory"])
- 
+
 
 # prints all files
 # print(dir_list)
 
-files = [] 
+files = []
 
 
 # def check_if_video(path):
@@ -40,10 +37,11 @@ files = []
 #     return ffmpeg.probe(path, select_streams = "v")['streams'][0]
 
 def getMetadata(filePath):
-    return ffmpeg.probe(filePath, select_streams = "v")['streams'][0]
+    return ffmpeg.probe(filePath, select_streams="v")['streams'][0]
+
 
 def fileHasVideoStream(file_path):
-     # video_stream = ffmpeg.probe(filePath, select_streams='v')['streams']
+    # video_stream = ffmpeg.probe(filePath, select_streams='v')['streams']
     try:
         video_stream = getMetadata(file_path)
         if video_stream:
@@ -55,15 +53,17 @@ def fileHasVideoStream(file_path):
         print(e.stdout)
         print("err")
         print(e.stderr)
-    
+
+
 def videoConvert(filePath, w, h, fps=25):
     try:
         input_vid = ffmpeg.input(filePath)
+        fileName = os.path.basename(filePath)
         return (
             input_vid
-            .filter('scale', w = w, h = h)
+            .filter('scale', w=w, h=h)
             .filter('fps', fps=fps, round='up')
-            .output("%sx%s_%s_output.mp4" % (w, h, fps))
+            .output("%sx%s_%s_%s" % (w, h, fps, fileName))
             .overwrite_output()
             .run()
         )
@@ -74,7 +74,7 @@ def videoConvert(filePath, w, h, fps=25):
         print("err")
         print(e.stderr)
 
-    
+
 # Listar os ficheiros de uma diretoria
 for entry in os.scandir(args["directory"]):
     if entry.is_file():
@@ -82,45 +82,31 @@ for entry in os.scandir(args["directory"]):
 
 # Processar os ficheiros
 for f in files:
-    if fileHasVideoStream(f):
-        metaData = getMetadata(f)
-        fileObj = {}
-        for l in CONFIGS:
-            fileObj[l] = metaData[l]
+    print(["python " + config.scriptLocation] + config.scriptParams)
+    subprocess.run(["python", "--version"])
 
-        for fps in FILTERS["fps"]:
-            for width in FILTERS["scale"]:
-                # print(width)
+    subprocess.run(["python", config.scriptLocation] + config.scriptParams)
 
-                videoConvert(f, width["w"], width["h"], fps)
-                # input_vid = ffmpeg.input(f)
-                # vid = (
-                #     input_vid
-                #     .filter('scale', w = width["w"], h = width["h"])
-                #     .filter('fps', fps=fps, round='up')
-                #     .output(str(fps) + str(width) + 'output.mp4')
-                #     .overwrite_output()
-                #     .run()
-                # )
+    # if fileHasVideoStream(f):
+    #     metaData = getMetadata(f)
+    #     fileObj = {}
+    #     for l in config.metadataProps:
+    #         fileObj[l] = metaData[l]
 
-        
+    #     for fps in config.fps:
+    #         for width in config.scale:
+    #             videoConvert(f, width["w"], width["h"], fps)
+    # subprocess.run(["python"] + [config.scriptLocation] + config.scriptParams)
 
-        # stream = ffmpeg.input(f)
-        # stream = ffmpeg.hflip(stream)
-        # stream = ffmpeg.output(stream, 'output.mp4')
-        # # ffmpeg.run(stream)
-        #  'width': 1024, 'height': 576, 'coded_width': 1024, 'coded_height': 576, 
-
-
-        # 'codec_name': 'h264',
-        # 'codec_long_name': 'H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10', 
-        # 'profile': 'High',
-        # 'display_aspect_ratio': '16:9',
-        # 'r_frame_rate': '25/1'
-        # 'avg_frame_rate': '25/1',
-        # duration
-        # 'duration_ts': 205290000, 'duration': '2281.000000',
-        # 'bit_rate': '2091719', 'bits_per_raw_sample': '8', 
+    # 'codec_name': 'h264',
+    # 'codec_long_name': 'H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10',
+    # 'profile': 'High',
+    # 'display_aspect_ratio': '16:9',
+    # 'r_frame_rate': '25/1'
+    # 'avg_frame_rate': '25/1',
+    # duration
+    # 'duration_ts': 205290000, 'duration': '2281.000000',
+    # 'bit_rate': '2091719', 'bits_per_raw_sample': '8',
 
 # https://github.com/kkroening/ffmpeg-python
 # https://github.com/kkroening/ffmpeg-python/tree/master/examples#audiovideo-pipeline
